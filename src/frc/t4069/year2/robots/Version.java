ea4ca9bed2a71e89d24e5e74f2d0d8143ec20aaf
Edmund Noble <edmundnoble@gmail.com>
2012-10-07 19:58:22 -0400

    Encoder preparation.

--- src/frc/t4069/year2/robots/Version.java
+++ src/frc/t4069/year2/robots/Version.java
@@ -2,5 +2,5 @@ package frc.t4069.year2.robots;
 
 public class Version {
 	public static final String author = "Edmund";
-	public static final String version = "0";
+	public static final String version = "\u03b1 0.01";
 }
